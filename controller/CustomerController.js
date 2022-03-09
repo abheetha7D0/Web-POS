fa3fcc8b35300de7a4d0d2dadfee3dccc5f150aa
abheetha7D0 <abeetha.dilushan@gmail.com>
2022-03-09 22:27:24 +0530

    customer save reddy

--- controller/CustomerController.js
+++ controller/CustomerController.js
@@ -1,15 +1,37 @@
 function loadAllCustomers() {
+    $("#customerTable").empty();
+    for (var i of customerDB) {
+        let row = `<tr><td>${i.getCustomerID()}</td><td>${i.getCustomerName()}</td><td>${i.getCustomerAddress()}</td><td>${i.getCustomerSalary()}</td></tr>`;
+        $("#customerTable").append(row);
+    }
+}
+
+function saveCustomer() {
+    var id = $("#inputCusId").val();
+    var name = $("#inputCustomerName").val();
+    var address = $("#inputCusAddress").val();
+    var salary = $("#inputCusSalary").val();
+
+    customerDB.push(new CustomerDTO(id, name, address, salary));
+}
+
+function deleteCustomer() {
 
 }
-function saveCustomer(){
+
+function updateCustomer() {
 
 }
-function deleteCustomer(){
 
-}
-function updateCustomer(){
-
-}
 function searchCustomer(id) {
 
 }
+
+
+$("#btnCusSave").click(function () {
+
+    saveCustomer();
+
+    loadAllCustomers();
+    $("#inputCusId,#inputCustomerName,#inputCusAddress,#inputCusSalary").val("");
+});
--- model/CustmerDTO.js
+++
@@ -1,7 +0,0 @@
-function CustomerDTO(id,name,address,salary){
-    var __id=id;
-    var __name=name;
-    var __address=address;
-    var __salary=salary;
-
-}
