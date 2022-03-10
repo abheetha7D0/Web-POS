33b7f58b01586d683b52dc42cfb9dcabb6123a9e
abheetha7D0 <abeetha.dilushan@gmail.com>
2022-03-10 11:47:45 +0530

    customer done

--- controller/CustomerController.js
+++ controller/CustomerController.js
@@ -3,6 +3,14 @@ function loadAllCustomers() {
     for (var i of customerDB) {
         let row = `<tr><td>${i.getCustomerID()}</td><td>${i.getCustomerName()}</td><td>${i.getCustomerAddress()}</td><td>${i.getCustomerSalary()}</td></tr>`;
         $("#customerTable").append(row);
+        $("#customerTable>tr").click(function () {
+            $('#inputSearchCus').val($(this).children(":eq(0)").text())
+
+            $('#inputUId').val($(this).children(":eq(0)").text())
+            $('#inputUCustomerName').val($(this).children(":eq(1)").text())
+            $('#inputUAddress').val($(this).children(":eq(2)").text())
+            $('#inputUSalary').val($(this).children(":eq(3)").text())
+        })
     }
 }
 
@@ -15,18 +23,49 @@ function saveCustomer() {
     customerDB.push(new CustomerDTO(id, name, address, salary));
 }
 
-function deleteCustomer() {
-
+function deleteCustomer(id) {
+    let customer;
+    if (id != null) {
+        for (var i = 0; i < customerDB.length; i++) {
+            if (id == customerDB[i].getCustomerID()) {
+                customer = customerDB[i];
+            }
+        }
+        let indexNumber = customerDB.indexOf(customer);
+        customerDB.splice(indexNumber, 1);
+        return true;
+    } else {
+        return false;
+    }
 }
 
 function updateCustomer() {
-
+    let customerID = $('#inputUId').val();
+    let customerName = $("#inputUCustomerName").val();
+    let customerAddress = $("#inputUAddress").val();
+    let customerSalary = $("#inputUSalary").val();
+    for (var i = 0; i < customerDB.length; i++) {
+        if (customerDB[i].getCustomerID() == $("#inputUId").val()) {
+            var customer = customerDB[i];
+            customer.setCustomerName(customerName);
+            customer.setCustomerAddress(customerAddress);
+            customer.setCustomeSalary(customerSalary)
+        }
+    }
 }
 
-function searchCustomer(id) {
+function searchCustomer() {
 
+    $("#customerTable").empty()
+    let cus;
+    for (var i = 0; i < customerDB.length; i++) {
+        if ($('#inputSearchCus').val() == customerDB[i].getCustomerID()) {
+            cus = customerDB[i];
+            let row = `<tr><td>${cus.getCustomerID()}</td><td>${cus.getCustomerName()}</td><td>${cus.getCustomerAddress()}</td><td>${cus.getCustomerSalary()}</td></tr>`;
+            $("#customerTable").append(row);
+        }
+    }
 }
-
 
 $("#btnCusSave").click(function () {
 
@@ -34,4 +73,35 @@ $("#btnCusSave").click(function () {
 
     loadAllCustomers();
     $("#inputCusId,#inputCustomerName,#inputCusAddress,#inputCusSalary").val("");
+});
+
+$("#btnCusDelete").click(function () {
+    let id = $('#inputSearchCus').val();
+    let option = confirm(`Do you want to delete ID:${id}`);
+    if (option) {
+        let erase = deleteCustomer(id);
+        if (erase) {
+            alert("Customer Deleted");
+            $('#inputCusID,#inputCusName,#inputCusAddress,#inputCusSalary').val("");
+        } else {
+            alert("Delete Failed , Try again");
+        }
+    }
+
+    loadAllCustomers();
+    $('#inputSearchCus').val("");
+});
+
+$("#btnCusUpdate").click(function () {
+    updateCustomer();
+    loadAllCustomers();
+    $('#inputCusID,#inputCusName,#inputCusAddress,#inputCusSalary').val("");
+});
+
+$("#btnCusSearch").click(function () {
+    searchCustomer();
+});
+$("#btnGetAllCus").click(function () {
+    loadAllCustomers();
+    $('#inputSearchCus').val("");
 });
